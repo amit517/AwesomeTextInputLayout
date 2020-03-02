3bf17efc62e745c7d8b0a8a62a8b0c187cd55af7
Amit Kundu <kundu.amit517@gmail.com>
2020-03-02 17:01:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/awesometextinputlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.awesometextinputlayout;
+
+//https://android-arsenal.com/details/1/8041
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
